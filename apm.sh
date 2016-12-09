8336abd4f8152b95da0f34b9f36846f13d7513c1
Cam Jackson <camjackson89@gmail.com>
2016-12-09 22:12:19 +1100

    Add a script to install atom packages

---
+++ apm.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+apm install \
+  linter \
+  linter-eslint \
+  hyperclick \
+  js-hyperclick \
+  react \
+  language-terraform \
+  linter-terraform-syntax \
+  expand-region
+
+echo "Don't forget to:"
+echo " 1. Turn on autosave"
+echo " 2. Turn on invisibles, etc"
+echo " 3. Turn on eslint autofix"
+echo " 4. Add a keymap for expand-region in ~/.atom/keymap.cson:"
+echo "'atom-text-editor':"
+echo "  'alt-up': 'expand-region:expand'"
+echo "  'alt-down': 'expand-region:shrink'"

a15b73627a295ee448010d4a2fe23e6dae613546
Cam Jackson <camjackson89@gmail.com>
2016-12-20 21:46:50 +1100

    Add language-docker, and console-log instructions

--- apm.sh
+++ apm.sh
@@ -13,12 +13,14 @@ apm install \
   git-blame \
   merge-conflicts \
   highlight-selected \
+  language-docker \
 
 echo "Don't forget to:"
 echo " 1. Turn on autosave"
 echo " 2. Turn on invisibles, etc"
 echo " 3. Turn on eslint autofix"
-echo " 4. Add a keymap for expand-region in ~/.atom/keymap.cson:"
+echo " 4. Turn off console-log context menus"
+echo " 5. Add a keymap for expand-region in ~/.atom/keymap.cson:"
 echo "'atom-text-editor':"
 echo "  'alt-up': 'expand-region:expand'"
 echo "  'alt-down': 'expand-region:shrink'"

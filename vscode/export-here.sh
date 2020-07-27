6157ce04a734dde2c51fa172000e96038c012dc6
Cam Jackson <camjackson89@gmail.com>
2020-07-27 13:34:09 +1000

    Add import script

--- vscode/export-here.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-code --list-extensions | xargs -L 1 echo code --install-extension > extensions.sh
-cp /Users/cam/Library/Application\ Support/Code/User/settings.json .
---
+++ vscode/import.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./install-extensions.sh
+cp settings.json /Users/cam/Library/Application\ Support/Code/User

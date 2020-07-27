fd73cdae280fea472e0bfbdcf2620dfb688f086a
Cam Jackson <camjackson89@gmail.com>
2020-07-27 10:12:40 +1000

    Add vscode icons

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -2,3 +2,4 @@ code --install-extension bradlc.vscode-tailwindcss
 code --install-extension esbenp.prettier-vscode
 code --install-extension rust-lang.rust
 code --install-extension svelte.svelte-vscode
+code --install-extension vscode-icons-team.vscode-icons

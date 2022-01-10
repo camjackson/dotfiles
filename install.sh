1e89331719808c7674d63dcac700f956c7e10a61
Cam Jackson <1930451+camjackson@users.noreply.github.com>
2022-01-10 15:53:19 +1100

    Add zshrc ln step

--- install.sh
+++ install.sh
@@ -4,6 +4,10 @@ for dotfile in .bashrc .bash_profile .gitconfig .gitignore_global .vimrc; do
   [ -e ~/$dotfile ] || ln -s "$(pwd)/$dotfile" ~
 done
 
+cd
+ln -s .bashrc .zshrc
+cd -
+
 mkdir -p ~/.vim
 [ -e ~/.vim/bundle/Vundle.vim ] ||  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 [ -e ~/.vim/syntax ] || ln -s "$(pwd)/.vim/syntax" ~/.vim/syntax
